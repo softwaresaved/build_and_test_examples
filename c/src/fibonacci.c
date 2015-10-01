4ffd4188221187a634bf2c773aa29a4f1f0714fc
Mike Jackson <mikezonk@gmail.com>
2015-10-01 12:03:06 +0100

    Fixed bug

--- c/src/fibonacci.c
+++ c/src/fibonacci.c
@@ -18,12 +18,10 @@ unsigned int fibonacci(unsigned int n)
 {
     if (n < 2)
     {
-        return 1;
-	//        return n;
+        return n;
     }
     else
     {
-      //        return fibonacci(n - 1) + fibonacci(n - 2);
-        return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
