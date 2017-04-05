86b3bcd3d1c50e4200dbbdaa3dcd09276dbb95c0
Garrett Gridley <gbg15@txstate.edu>
2017-04-05 18:09:16 -0500

    Added quoted.txt files and renamed hellogarrett.py

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ echo "testing hwInput_GP"
 python hello.py < hwInput_GP.txt
 echo "testing hwInput_GP_quoted"
 python hello.py < hwInput_GP_quoted.txt
-echo "testomg hwInput_2"
+echo "testing hwInput_2"
 python hello.py < hwInput_2.txt
-echo "testomg hwInput_2_quoted"
+echo "testing hwInput_2_quoted"
 python hello.py < hwInput_2_quoted.txt
