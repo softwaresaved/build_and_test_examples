00592cb9788661f21a8fd7d23de2ca3a8aa565b6
treycalvertsp2017 <ttc12@txstate.edu>
2017-04-05 17:45:32 -0500

    updated

--- helloworld.py
+++ helloworld.py
@@ -3,12 +3,8 @@ print ("Trey's my name getting A's my trade... hopefully")
 
 
 print("Hello Team, It is Matt")
-
-<<<<<<< HEAD
-print(" d-_-b ")
+
 
 print("Hey Rust, this is James.")
 
-=======
-print(" d-_-b ")
->>>>>>> refs/remotes/origin/master
+
