593263b4f091c807d24c42fdede91c242926d7ec
Isaac Bondoc <isaac.bondoc@gmail.com>
2017-04-05 18:18:29 -0500

    This should build
    Isaac B.
    
    -_-

--- helloworld.py
+++ helloworld.py
@@ -3,25 +3,10 @@ print ("Trey's my name getting A's my trade... hopefully")
 
 print("Hello Team, It is Matt")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-print(" d-_-b ")
+print("Hey Rust, this is Isaac")
 
 print("Hey Rust, this is James.")
 
-=======
-print(" d-_-b ")
->>>>>>> 8a024406ab13d9de27b4416de4453e1d19f031af
-=======
-
-print("Hey Rust, this is James.")
-
-
-Print("Yooo") 
->>>>>>> bef2a66681dc8124999e0bc91be56c83b0a46492
-=======
-print("Hey Rust, this is James.")
 
 print("Yooo") 
 
@@ -34,4 +19,3 @@ except NameError:
     print("That was neither a numeral or a string\n")
 #except SyntaxError:
     #print("Syntax Error:  Possibly an incorrectly formatted string\n")
->>>>>>> b6669d9e914bc9c04984fb3ba05aff3f90b0f9ce
