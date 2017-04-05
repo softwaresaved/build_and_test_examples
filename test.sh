33b23cd70024811fb6665a00b32a5c1e9bbb71c3
Elliot <ewhite29@gmail.com>
2017-04-05 18:41:25 -0500

    Cleaned up

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 try: 
 	mode=int(input("Give me a number!" ))
 	print("That number was: "), mode, "\n"
@@ -8,9 +8,9 @@ except NameError:
 	print("That was neither a number or a string\n")
 
 # Isaac was here.
-=======
+
 python helloworld.py < hwInput_GP.txt
 python helloworld.py < hwInput_GP_quoted.txt
 python helloworld.py < hwInput_2.txt
 python helloworld.py < hwInput_2_quoted.txt
->>>>>>> b6669d9e914bc9c04984fb3ba05aff3f90b0f9ce
+
