a30c4ec7cb3741616485e274c022302223d66e7e
Garrett Gridley <gbg15@txstate.edu>
2017-04-05 17:50:10 -0500

    Adding helloGarrett

---
+++ test.sh
@@ -0,0 +1,8 @@
+echo "testing hwInput_GP"
+python helloworld.py < hwInput_GP.txt
+echo "testing hwInput_GP_quoted"
+python helloworld.py < hwInput_GP_quoted.txt
+echo "testomg hwInput_2"
+python helloworld.py < hwInput_2.txt
+echo "testomg hwInput_2_quoted"
+python helloworld.py < hwInput_2_quoted.txt
