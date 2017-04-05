5132569a91de925255a0bc4e5227eaa1c32914dc
Isaac Bondoc <isaac.bondoc@gmail.com>
2017-04-05 18:24:42 -0500

    THIS SHOULD WORK
    asdfasdf

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,7 @@ print ("Trey's my name getting A's my trade... hopefully")
 
 print("Hello Team, It is Matt")
 
-print("Hey Rust, this is Isaac")
+print("Hey Rust, this is Isaac ")
 
 print("Hey Rust, this is James.")
 
