6c672f3775ce4446ac529cb9f3dee298cbfed7b3
ZhenyaHanson <edh43@txstate.edu>
2017-04-05 23:19:39 -0500

    Another edit

--- hellogarrett.py
+++ hellogarrett.py
@@ -4,3 +4,7 @@ print("******************")
 print("***edited*by*****")
 print("*****cody*********")
 print("******************")
+
+# Another edit by Zhenya
+print("I am an edit. I was created by Zhenya (Eugene Hanson)")
+print("Who knows the places I may go.")
